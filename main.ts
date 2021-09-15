02927899354d5bfa437e142cf57a5d8332e74dc3
Ayansh25op <87590978+Ayansh25op@users.noreply.github.com>
2021-09-15 18:57:58 +0530

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,16 +2,16 @@ let isSwitched = false
 let force = 0
 input.onButtonPressed(Button.A, function () {
     if (isSwitched) {
-        basic.showString("B")
-    } else {
         basic.showString("A")
+    } else {
+        basic.showString("B")
     }
 })
 input.onButtonPressed(Button.B, function () {
     if (isSwitched) {
-        basic.showString("A")
-    } else {
         basic.showString("B")
+    } else {
+        basic.showString("A")
     }
 })
 basic.forever(function () {
